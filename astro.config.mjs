6739f5ad02b8e23772bdb585edce3616def32324
Mikle Seregin <mikhail-seregin@yandex.ru>
2025-04-11 17:17:12 +0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    devToolbar: {
-        enabled: false
-    }
-});
+export default defineConfig({});
